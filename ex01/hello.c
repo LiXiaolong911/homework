48b503e1aaebe37d85a783a4a3bcc696e3715cd3
LiXiaolong911 <21006598905@qq.com>
2026-06-04 20:06:12 +0800

    更新 README 和 hello.c 程序

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World\n");
+	printf("Hello World\n");
+	char name[100];  // 存储用户输入的字符串（最多100个字符）
+    	printf("请输入一些内容：");
+    	scanf("%s", name);  // 读取用户输入的字符串（以空格或回车结束）
+    	printf("你输入的内容是：%s\n", name);
     return 0;
 }
